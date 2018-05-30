dfcedfa7b0630b0d831e9bb501ebcb169fa489e3
KennyHPL <32531981+KennyHPL@users.noreply.github.com>
2018-05-30 01:21:20 -0700

    retr

--- WaterRipple/ripple.py
+++ WaterRipple/ripple.py
@@ -3,11 +3,11 @@ import numpy as np
 import threading
 import time
 
-WIDTH = 150; HEIGHT = 150
+WIDTH = 100; HEIGHT = 100
 dampening = .99
 done = False
 
-def calcBuffer(buffer2, buffer1, endRow, endCol, startRow, startCol):
+def calcBuffer(buffer2, buffer1, startRow, endRow, startCol, endCol):
     for i in range(startRow, endRow):
         for j in range(startCol, endCol):
             buffer2[i][j] = ((buffer1[i-1][j]+
@@ -15,7 +15,7 @@ def calcBuffer(buffer2, buffer1, endRow, endCol, startRow, startCol):
                              buffer1[i][j-1]+
                              buffer1[i][j+1])/2 - buffer2[i][j]) * dampening
 
-def setScreen(screen, buffer2, endRow, endCol, startRow, startCol):
+def setScreen(screen, buffer2, startRow, endRow, startCol, endCol):
     for i in range(startRow, endRow):
         for j in range(startCol, endCol):
             color = (buffer2[i][j])
@@ -47,27 +47,47 @@ while not done:
         buffer1[pos[0]][pos[1]] = 255
         pressed = True
 
+    before = time.time()
     t1 = threading.Thread(target =calcBuffer,
-                          args=(buffer2, buffer1, WIDTH//2, HEIGHT//2, 1, 1))
+                          args=(buffer2, buffer1, 1, WIDTH//2, 1, HEIGHT//2))
     t2 = threading.Thread(target =calcBuffer,
-                          args=(buffer2, buffer1, WIDTH-1, HEIGHT-1, WIDTH//2+1, HEIGHT//2))
+                          args=(buffer2, buffer1, WIDTH//2, WIDTH-1, HEIGHT//2, HEIGHT-1))
     t3 = threading.Thread(target =calcBuffer,
-                          args=(buffer2, buffer1, WIDTH-1, HEIGHT//2, WIDTH//2+1, 1))
+                          args=(buffer2, buffer1, WIDTH//2, WIDTH-1, 1, HEIGHT//2))
     t4 = threading.Thread(target =calcBuffer,
-                          args=(buffer2, buffer1, WIDTH//2, HEIGHT-1, 1, HEIGHT//2))
+                          args=(buffer2, buffer1, 1, WIDTH//2, HEIGHT//2, HEIGHT-1))
     t1.start();t2.start();t3.start();t4.start()
     t1.join();t2.join();t3.join();t4.join()
 
     t1 = threading.Thread(target =setScreen,
-                          args=(screen, buffer2, WIDTH//2, HEIGHT//2, 1, 1))
+                          args=(screen, buffer2, 0, WIDTH//2, 0, HEIGHT//2))
     t2 = threading.Thread(target =setScreen,
-                          args=(screen, buffer2, WIDTH, HEIGHT, WIDTH//2, HEIGHT//2))
+                          args=(screen, buffer2, WIDTH//2, WIDTH, HEIGHT//2, HEIGHT))
     t3 = threading.Thread(target =setScreen,
-                          args=(screen, buffer2, WIDTH, HEIGHT//2, WIDTH//2, 1))
+                          args=(screen, buffer2, WIDTH//2, WIDTH, 0, HEIGHT//2))
     t4 = threading.Thread(target =setScreen,
-                          args=(screen, buffer2, WIDTH//2, HEIGHT, 1, HEIGHT//2))
+                          args=(screen, buffer2, 0, WIDTH//2, HEIGHT//2, HEIGHT))
     t1.start();t2.start();t3.start();t4.start()
     t1.join();t2.join();t3.join();t4.join()
+
+    # for i in range(1, WIDTH-1):
+    #     for j in range(1, HEIGHT-1):
+    #         buffer2[i][j] = ((buffer1[i-1][j]+
+    #                          buffer1[i+1][j]+
+    #                          buffer1[i][j-1]+
+    #                          buffer1[i][j+1])/2 - buffer2[i][j]) * dampening
+    # for i in range(WIDTH):
+    #     for j in range(HEIGHT):
+    #         color = (buffer2[i][j])
+    #         if color > 255:
+    #             color = 255
+    #         elif color < 0:
+    #             color = 0
+    #         screen.set_at((i,j), (color,color,color))
+
+    after = time.time()
+    print(after-before)
+
 
     temp = buffer2
     buffer2 = buffer1
